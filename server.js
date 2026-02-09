f5aa1713757eb47fcd47a4209ada89366caef4b1
HectordelRio <AL07020940@tecmilenio.mx>
2026-02-08 20:59:21 -0600

    servir frontend desde express

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 
 // Rutas
 app.use('/api/auth', require('./routes/auth'));
